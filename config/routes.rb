2e0fbb1be490de3c57d659e04705144370970327
Gaston Montiel <gaston.montiel@these.com.uy>
2025-06-15 23:06:01 -0300

    [user profile] el usuario peude editar sus datos

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  resources :user_profile, only: [ :show, :edit, :update ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
   root to: "home#home"
 
