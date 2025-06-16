0650ef797bc2fb10ae95cb3401357b01600e716f
Gaston Montiel <gaston.montiel@these.com.uy>
2025-06-15 21:11:56 -0300

    [users] admin links

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,30 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_admin!
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def authorize_admin!
+    redirect_to root_path, alert: "Access denied." unless current_user.has_role? :admin
   end
 end
