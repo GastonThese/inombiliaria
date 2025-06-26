c74d571fbb75697b3fabb98dda56ef4fb2eefe7c
Gaston Montiel <gaston.montiel@these.com.uy>
2025-06-25 22:43:42 -0300

    [correcion error en consola]

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -4,17 +4,23 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     let savedTheme = localStorage.getItem('theme');
-    
+
+    const navbar = document.getElementById('navbar');
+
     if (savedTheme === 'dark') {
       document.documentElement.classList.add('dark');
+
+      if (!navbar) return;
+
       document.getElementById('moon').classList.add('hidden');
       document.getElementById('sun').classList.remove('hidden');
     } else {
+
+      if (!navbar) return;
+
       document.getElementById('moon').classList.remove('hidden');
       document.getElementById('sun').classList.add('hidden');
     }
-
-    ;
   }
 
   changeTheme() {
