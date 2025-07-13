4668e61b02b3f1be73f2e7f94461de90c8209ca7
Gaston Montiel <gaston.montiel@these.com.uy>
2025-07-13 12:40:50 -0300

    [user] refactor

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User, type: :model do
   it { expect(subject).to validate_presence_of(:email) }
   it { expect(subject).to validate_presence_of(:password) }
   it { expect(subject).to have_many(:properties) }
-  it { expect(subject).to have_one(:rented_unit) }
+  it { expect(subject).to have_one(:rented_property) }
 
   context "user utilities" do
     let!(:owner) { create(:user_owner_with_units) }
 
     it 'how to get all tenants' do
-      expect(User.joins(:rented_unit).uniq.count).to eq(10)
+      expect(User.joins(:rented_property).uniq.count).to eq(10)
     end
 
     it 'how to get all properties form owner' do
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
     end
 
     it 'how to get tenant rented unit / real state' do
-      unitId = User.with_role(:tenant).first.rented_unit.id
+      unitId = User.with_role(:tenant).first.rented_property.id
       expect(unitId).to eq(Property.find(unitId).id)
     end
   end
