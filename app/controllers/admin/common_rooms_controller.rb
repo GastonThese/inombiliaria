204c9d2e639ebc55450efad844703ea50f6cec9a
Gaston Montiel <gaston.montiel@these.com.uy>
2025-07-21 23:38:29 -0300

    [common room] implementando salones comunes

--- app/controllers/admin/common_rooms_controller.rb
+++ app/controllers/admin/common_rooms_controller.rb
@@ -1,6 +1,10 @@
 class Admin::CommonRoomsController < ApplicationController
   load_and_authorize_resource
   def show
+    @common_room = CommonRoom.find(params[:id])
+    @building = @common_room.building
+  rescue ActiveRecord::RecordNotFound
+    redirect_to admin_buildings_path, alert: 'Sala comunal no encontrada.'
   end
 
   def new
@@ -22,9 +26,23 @@ class Admin::CommonRoomsController < ApplicationController
   end
 
   def edit
+    @common_room = CommonRoom.find(params[:id])
+    @building = @common_room.building
+  rescue ActiveRecord::RecordNotFound
+    redirect_to admin_buildings_path, alert: 'Sala comunal no encontrada.'
   end
 
   def update
+    @common_room = CommonRoom.find(params[:id])
+    @common_room.building = Building.find(params[:building_id])
+
+    if @common_room.update(common_room_params)
+      redirect_to admin_building_path(params[:building_id]), notice: 'Se ha actualizado la sala comunal exitosamente.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to admin_buildings_path, alert: 'Edificio no encontrado.'
   end
 
   private
